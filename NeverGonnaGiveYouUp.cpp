926bbbc434932b0ff859d9dbdbeaffe5cf04bd59
Yang-Tianming <77971770+Yang-Tianming@users.noreply.github.com>
2022-07-31 18:49:22 +0800

    Add files via upload

---
+++ NeverGonnaGiveYouUp.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+
+int main()
+{
+	MessageBox(0,"We're no strangers to love","你被骗了",MB_OK);
+	MessageBox(0,"You know the rules and so do I","你被骗了",MB_OK);
+	MessageBox(0,"A full commitment's what I'm thinking of","你被骗了",MB_OK);
+	MessageBox(0,"You wouldn't get this from any other guy","你被骗了",MB_OK);
+	MessageBox(0,"I just wanna tell you how I'm feeling","你被骗了",MB_OK);
+	MessageBox(0,"Gotta make you understand","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna give you up","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna let you down","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna run around and desert you","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna make you cry","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna say goodbye","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna tell a lie and hurt you","你被骗了",MB_OK);
+	MessageBox(0,"We've known each other for so long","你被骗了",MB_OK);
+	MessageBox(0,"Your heart's been aching but you're too shy to say it","你被骗了",MB_OK);
+	MessageBox(0,"Inside we both know what's been going on","你被骗了",MB_OK);
+	MessageBox(0,"We know the game and we're gonna play it","你被骗了",MB_OK);
+	MessageBox(0,"And if you ask me how I'm feeling","你被骗了",MB_OK);
+	MessageBox(0,"Don't tell me you're too blind to see","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna give you up","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna let you down","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna run around and desert you","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna make you cry","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna say goodbye","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna tell a lie and hurt you","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna give you up","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna let you down","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna run around and desert you","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna make you cry","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna say goodbye","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna tell a lie and hurt you","你被骗了",MB_OK);
+	MessageBox(0,"We've known each other for so long","你被骗了",MB_OK);
+	MessageBox(0,"Your heart's been aching but you're too shy to say it","你被骗了",MB_OK);
+	MessageBox(0,"Inside we both know what's been going on","你被骗了",MB_OK);
+	MessageBox(0,"We know the game and we're gonna play it","你被骗了",MB_OK);
+	MessageBox(0,"I just wanna tell you how I'm feeling","你被骗了",MB_OK);
+	MessageBox(0,"Gotta make you understand","你被骗了",MB_OK);
+	MessageBox(0,"Inside we both know what's been going on","你被骗了",MB_OK);
+	MessageBox(0,"Inside we both know what's been going on","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna give you up","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna let you down","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna run around and desert you","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna make you cry","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna say goodbye","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna tell a lie and hurt you","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna give you up","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna let you down","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna run around and desert you","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna make you cry","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna say goodbye","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna tell a lie and hurt you","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna give you up","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna let you down","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna run around and desert you","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna make you cry","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna say goodbye","你被骗了",MB_OK);
+	MessageBox(0,"Never gonna tell a lie and hurt you","你被骗了",MB_OK);
+	return 0;
+} 
